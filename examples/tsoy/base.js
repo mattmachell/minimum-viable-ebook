ccc57d462803be8617374e1b026eb4b5b29e5476
Matt <matt@realms.co.uk>
2013-02-17 22:52:24 +0000

    updated to include iOS flyout fix

---
+++ examples/tsoy/base.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function(){
+    var addFocus=function(){
+        this.setAttribute('class',this.getAttribute('class')+' focus');
+        this.removeEventListener('touchstart', addFocus, false);
+        this.addEventListener('touchstart', removeFocus, false);
+        }
+        
+    var removeFocus=function(){
+        this.setAttribute('class',this.getAttribute('class').replace(' focus','')) ;
+        this.removeEventListener('touchstart', removeFocus, false);
+        this.addEventListener('touchstart', addFocus, false);
+        
+    }    
+    document.getElementById('table-of-contents').addEventListener('touchstart', addFocus, false);
+});
